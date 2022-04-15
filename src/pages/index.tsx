b6c01d69e43a085729437db9f765294229b2c768
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-04-15 14:03:58 -0300

    Global Style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import styles from "../styles/home.module.scss"
 
 export default function Home() {
-  return <h1 className={styles.title}>Olá <span>Dev!</span></h1>;
+  return <h1>Olá <span>Dev!</span></h1>;
 }
