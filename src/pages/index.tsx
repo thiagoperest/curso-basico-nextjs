f19db88e20b6e5be773bae837fd8b4a1a1a7a586
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-04-15 19:27:07 -0300

    Client Side Rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
-import styles from "../styles/home.module.scss"
+import { useEffect, useState } from "react";
+
+interface Post {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
-  return <h1>Olá <span>Dev!</span></h1>;
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/posts").then((response) => {
+      response.json().then((data) => {
+        setPosts(data);
+      });
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
